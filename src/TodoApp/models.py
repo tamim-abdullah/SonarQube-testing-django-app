8bc07b6f9c60009f95a20e920ae05ecc000fad4b
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-08 19:54:50 +0530

    Added migrations

--- src/TodoApp/models.py
+++ src/TodoApp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 class Todo(models.Model):
     task = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100) 
+    is_completed = models.BooleanField(default = False)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.task
