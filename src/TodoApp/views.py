cf7c9478f2ffbe5b47ed48b8b93194d48ca61dbf
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-09 15:18:23 +0530

    Added validate form

--- src/TodoApp/views.py
+++ src/TodoApp/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Todo
+from .forms import TodoForm
 
 def index(request):
     tasks = Todo.objects.all()
+    form = TodoForm()
+    
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()     
+        return redirect('/')
+            
+    
     context = {
-        'tasks' : tasks
+        'tasks' : tasks,
+        'form' : form
     }
     return render(request , 'TodoApp/list.html' , context)
