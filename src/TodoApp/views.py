cb0ac7c7de6290444798341dcfdaf71fc5252963
adityaShar24 <adityarajansharma1234@gmail.com>
2023-11-18 14:10:28 +0530

    Added is authenticated

--- src/TodoApp/views.py
+++ src/TodoApp/views.py
@@ -57,38 +57,46 @@ def delete_task(request , pk):
 
 
 def register_user(request):
-    form = CreateUserForm()
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request , user + ' has been registered successfully!')
-
-            return redirect('login')
+    if user.is_authenticated:
+        return redirect('/')
+    else:    
+        form = CreateUserForm()
         
-    context = {'form':form}
-    return render(request , 'TodoApp/register.html' , context)
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request , user + ' has been registered successfully!')
+
+                return redirect('login')
+            
+        context = {'form':form}
+        return render(request , 'TodoApp/register.html' , context)
 
 def login_user(request):
-    form = CreateUserForm(request.POST)
+    
+    if user.is_authenticated:
+        return redirect('/')
+    
+    else:
+        form = CreateUserForm(request.POST)
+        
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password1')
+            
+            user = authenticate(request , username=username , password=password)
+            
+            if user is not None:
+                login(request , user)
+                return redirect('/')
+            else:
+                messages.info(request , 'Credentials are incorrect!')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password1')
-        
-        user = authenticate(request , username=username , password=password)
-        
-        if user is not None:
-            login(request , user)
-            return redirect('/')
-        else:
-            messages.info(request , 'Credentials are incorrect!')
-
-        
-    context = {'form':form}
-    return render(request , 'TodoApp/login.html' , context)
+            
+        context = {'form':form}
+        return render(request , 'TodoApp/login.html' , context)
 
 def logout_user(request):
     logout(request)
