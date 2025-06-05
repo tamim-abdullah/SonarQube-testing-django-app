43b4ea2031432c3f698097e9f49fa48529ca8a92
unknown <ta.prgmr@gmail.com>
2025-06-05 20:36:23 +0600

    test case added

--- src/TodoApp/tests.py
+++ src/TodoApp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+class TodoModelTest(TestCase):
+    def test_todo_creation(self):
+        todo = Todo.objects.create(
+            task="Test Task",
+            description="Test Description",
+            is_completed=False
+        )
+        self.assertEqual(todo.task, "Test Task")
+        self.assertEqual(todo.description, "Test Description")
+        self.assertFalse(todo.is_completed)
+        self.assertTrue(todo.created)  # Check created timestamp is set
+        self.assertEqual(str(todo), "Test Task")  # Test __str__ method
